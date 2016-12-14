93a7b0679d1bf8bfc8a3cc9002a5b2bc32d90e13
Jesus <gesusjl@gmail.com>
2016-12-14 18:36:38 -0500

    finish chapter 11

--- _config.py
+++ _config.py
@@ -3,12 +3,13 @@ import os
 # grab the folder where this script lives
 basedir = os.path.abspath(os.path.dirname(__file__))
 DATABASE = 'flasktaskr.db'
-USERNAME = 'admin'
-PASSWORD = 'admin'
-WTF_CSRF_ENABLED = True
+CSRF_ENABLED = True
 # use random key generator instead
 # https://github.com/rjw57/hdcp-genkey
 SECRET_KEY = "my_precious"
 
 # define the full path for the database
 DATABASE_PATH = os.path.join(basedir, DATABASE)
+
+# the database uri
+SQLALCHEMY_DATABASE_URI = 'sqlite:////' + DATABASE_PATH
--- forms.py
+++ forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import Form
 from wtforms import StringField, DateField, IntegerField, \
-    SelectField
-from wtforms.validators import DataRequired
+    SelectField, PasswordField
+from wtforms.validators import DataRequired, Length, EqualTo
 
 
 class AddTaskForm(Form):
     task_id = IntegerField()
-    name = StringField('Task Name', validators=[DataRequired])
+    name = StringField('Task Name', validators=[DataRequired()])
     due_date = DateField(
         'Date Due (mm/dd/yyyy)',
         validators=[DataRequired()], format='%m/%d/%Y'
@@ -20,3 +20,34 @@ class AddTaskForm(Form):
         ]
     )
     status = IntegerField('Status')
+
+
+class RegisterForm(Form):
+    name = StringField(
+        'Username',
+        validators=[DataRequired(), Length(min=6, max=25)]
+    )
+    email = StringField(
+        'Email',
+        validators=[DataRequired(), Length(min=6, max=40)]
+    )
+    password = PasswordField(
+        'Password',
+        validators=[DataRequired(), Length(min=6, max=40)]
+    )
+    confirm = PasswordField(
+        'Repeat password',
+        validators=[DataRequired(), EqualTo('password',
+                    message='Passwords must match')]
+    )
+
+
+class LoginForm(Form):
+    name = StringField(
+        'Username',
+        validators=[DataRequired()]
+    )
+    password = PasswordField(
+        'Password',
+        validators=[DataRequired()]
+    )
